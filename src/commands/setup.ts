c871b0e431761e84e9645f67519be55409800177
_Pesto <pestonotpasta@gmail.com>
2022-10-17 18:30:29 -0700

    Setup command

---
+++ src/commands/setup.ts
@@ -0,0 +1,37 @@
+import { Command } from 'types/command';
+import { hasGuildConfig } from 'handlers/GuildConfigurationHandler';
+import { ApplicationCommandOptionType, EmbedBuilder } from 'discord.js';
+import { genericEmbed } from 'utils/discord_helper';
+export const SetupCommand: Command = {
+    name: 'setup',
+    description: 'Starts the setup wizard',
+    options: [
+        {
+            name: 'override',
+            description: '',
+            type: ApplicationCommandOptionType.Boolean
+        }
+    ],
+
+    async handle(interaction) {
+        const guildId = interaction.guild?.id;
+        const override = interaction.options.getBoolean('override');
+        
+        if (!hasGuildConfig(guildId!) || override) {
+                    
+
+        } else {
+            const embed = genericEmbed('An configuration for this server already exists. If you\'d like to override the current configuration, please execute the command again with the override flag.')
+            await interaction.reply({ ephemeral: true, embeds: [embed] })
+            return;
+        }
+        
+
+        
+
+        
+
+
+
+    },
+}
